878ced3a807d18a64ce7b93a588609839adc4a58
Moelaal <medo.elaal@gmail.com>
2022-02-24 17:54:57 +0200

    names

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-const names = ['John','Jemy','jakov','mando','mohamed','adel','jommana','Jehan','jehad','moataz','said','nader','fady']
+const names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"]
 
 for ( let i = 0 ; i < names.length ; i++){
     if (names[i].charAt(0) === 'j' || names[i].charAt(0) === 'J'){
-        console.log(`goodbye ${names[i]}`);
+        console.log(`Goodbye ${names[i]}`);
     }else{
         console.log(`Hello ${names[i]}`);
     }
